099a4f1273118dd33abee85bb02f968fe4eab158
Kita <ghub@tkita.net>
2025-06-29 11:43:41 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ else:
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("ここに入力してください"):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
